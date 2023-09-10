e5581374e0117f9de2a4a3cff96b7de4b733aede
intimyp <intimyp@gmail.com>
2023-09-10 15:02:49 +0200

    Add alphabet

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ const defaultCardsData: CardData[] = [
 ];
 const local = localStorage.getItem("cardsData");
 const localCardsData = local ? JSON.parse(local) : [];
-const startCardsData = localCardsData.length > 0 ? localCardsData : defaultCardsData;
+const startCardsData =
+  localCardsData.length > 0 ? localCardsData : defaultCardsData;
 
 function useCardsData() {
   const [cardsData, setCardsData] = useState<CardData[]>(startCardsData);
@@ -27,7 +28,7 @@ function useCardsData() {
   return { cardsData, setCardsData, removeCard, addCard, editCard } as const;
 }
 
-type AllowedState = "import" | "view";
+type AllowedState = "import" | "view" | "alphabet";
 
 interface NavBarProps {
   name: string;
@@ -51,9 +52,10 @@ function NavBar({ children, name }: PropsWithChildren<NavBarProps>) {
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          {children && (children as ReactNode[]).map((child, i) => (
-            <li key={i}>{child}</li>
-          ))}
+          {children &&
+            (children as ReactNode[]).map((child, i) => (
+              <li key={i}>{child}</li>
+            ))}
         </ul>
       </div>
     </div>
@@ -213,6 +215,278 @@ function Import({ setCardsData, setState }: ImportProps) {
   );
 }
 
+interface LetterInfo {
+  alone: string;
+  initial: string;
+  middle: string;
+  final: string;
+  english: string;
+  description: string;
+}
+
+const letters: LetterInfo[] = [
+  {
+    alone: "ب",
+    initial: "بـ",
+    middle: "ـبـ",
+    final: "ـب",
+    english: "b",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "پ",
+    initial: "پـ",
+    middle: "ـپـ",
+    final: "ـپ",
+    english: "p",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ت",
+    initial: "تـ",
+    middle: "ـتـ",
+    final: "ـت",
+    english: "t",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ج",
+    initial: "جـ",
+    middle: "ـجـ",
+    final: "ـج",
+    english: "j",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "چ",
+    initial: "چـ",
+    middle: "ـچـ",
+    final: "ـچ",
+    english: "ch",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "خ",
+    initial: "خـ",
+    middle: "ـخـ",
+    final: "ـخ",
+    english: "kh",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "د",
+    initial: "د",
+    middle: "ـد",
+    final: "ـد",
+    english: "d",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ر",
+    initial: "ر",
+    middle: "ـر",
+    final: "ـر",
+    english: "r",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ڕ",
+    initial: "ڕ",
+    middle: "ـڕ",
+    final: "ـڕ",
+    english: "r",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ز",
+    initial: "ز",
+    middle: "ـز",
+    final: "ـز",
+    english: "z",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ژ",
+    initial: "ژ",
+    middle: "ـژ",
+    final: "ـژ",
+    english: "zh",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "س",
+    initial: "سـ",
+    middle: "ـسـ",
+    final: "ـس",
+    english: "s",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ش",
+    initial: "شـ",
+    middle: "ـشـ",
+    final: "ـش",
+    english: "sh",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ف",
+    initial: "فـ",
+    middle: "ـفـ",
+    final: "ـف",
+    english: "f",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ڤ",
+    initial: "ڤـ",
+    middle: "ـڤـ",
+    final: "ـڤ",
+    english: "v",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "گ",
+    initial: "گـ",
+    middle: "ـگـ",
+    final: "ـگ",
+    english: "g",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ک",
+    initial: "کـ",
+    middle: "ـکـ",
+    final: "ـک",
+    english: "k",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ق",
+    initial: "قـ",
+    middle: "ـقـ",
+    final: "ـق",
+    english: "q",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ل",
+    initial: "لـ",
+    middle: "ـلـ",
+    final: "ـل",
+    english: "l",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ڵ",
+    initial: "ڵـ",
+    middle: "ـڵـ",
+    final: "ـڵ",
+    english: "l",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "م",
+    initial: "مـ",
+    middle: "ـمـ",
+    final: "ـم",
+    english: "m",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ن",
+    initial: "نـ",
+    middle: "ـنـ",
+    final: "ـن",
+    english: "n",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ه",
+    initial: "هـ",
+    middle: "ـهـ",
+    final: "ـه",
+    english: "h",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "و",
+    initial: "و",
+    middle: "ـو",
+    final: "ـو",
+    english: "w",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ۆ",
+    initial: "ۆ",
+    middle: "ـۆ",
+    final: "ـۆ",
+    english: "o",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ی",
+    initial: "ی",
+    middle: "ـی",
+    final: "ـی",
+    english: "y",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ێ",
+    initial: "ێ",
+    middle: "ـێ",
+    final: "ـێ",
+    english: "e",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ا",
+    initial: "ئا",
+    middle: "ـا",
+    final: "ـا",
+    english: "a",
+    description: "pronounced like in english",
+  },
+  {
+    alone: "ە",
+    initial: "ئە",
+    middle: "ـە",
+    final: "ـە",
+    english: "e",
+    description: "pronounced like in english",
+  },
+];
+
+function Alphabet() {
+  return (
+    <div className="flex flex-wrap">
+      {letters.map((letter, i) => (
+        <label className="swap swap-flip">
+          <input type="checkbox" />
+          <div key={i} className="card bordered w-40 h-40 swap-on">
+            <div className="card-body">
+              <h2 className="card-title">{letter.english}</h2>
+              <div className="flex flex-row-reverse">
+                <p>{letter.initial}</p>
+                <p>{letter.middle}</p>
+                <p>{letter.final}</p>
+                <p>{letter.alone}</p>
+              </div>
+            </div>
+          </div>
+          <div key={i} className="card bordered w-40 h-40 swap-off">
+            <div className="card-body">
+              <h2 className="card-title">{letter.alone}</h2>
+            </div>
+          </div>
+        </label>
+      ))}
+    </div>
+  );
+}
+
 function App() {
   const [state, setState] = useState<AllowedState>("view");
   const { cardsData, setCardsData, removeCard, addCard, editCard } =
@@ -220,11 +494,10 @@ function App() {
   return (
     <div>
       <NavBar name="سۆرانی">
-          <button onClick={() => setState("import")}>Import</button>
-          <button onClick={() => exportCardsDatatoJSON(cardsData)}>
-            Export
-          </button>
-          <button onClick={() => setState("view")}>View</button>
+        <button onClick={() => setState("import")}>Import</button>
+        <button onClick={() => exportCardsDatatoJSON(cardsData)}>Export</button>
+        <button onClick={() => setState("view")}>View</button>
+        <button onClick={() => setState("alphabet")}>Alphabet</button>
       </NavBar>
       <div className="container mx-auto">
         {state === "view" ? (
@@ -238,6 +511,7 @@ function App() {
         {state === "import" ? (
           <Import setCardsData={setCardsData} setState={setState} />
         ) : null}
+        {state === "alphabet" ? <Alphabet /> : null}
       </div>
     </div>
   );
