11ace5b05c250813cd619955bc0336a82c00da2e
intimyp <intimyp@gmail.com>
2023-09-14 21:28:22 +0200

    Add responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -47,10 +47,21 @@ function exportCardsDatatoJSON(cardsData: CardData[]) {
 function NavBar({ children, name }: PropsWithChildren<NavBarProps>) {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            {children &&
+              (children as ReactNode[]).map((child, i) => (
+                <li key={i}>{child}</li>
+              ))}
+          </ul>
+        </div>
         <a className="btn btn-ghost normal-case text-xl">{name}</a>
       </div>
-      <div className="flex-none">
+      <div className="navbar-end hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           {children &&
             (children as ReactNode[]).map((child, i) => (
